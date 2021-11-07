55b11bd3b7576295740e1fa27c3be42e37d35753
Rocco Luca Iamello s282145 <roccoluca@hotmail.it>
2021-11-07 10:23:13 +0100

    Refactor, unique test folder

---
+++ test/dao.test.js
@@ -0,0 +1,28 @@
+const dao = require('../server/dao');
+
+//TEST DAO FUNCTION CREATE BOOKING
+test('the creation fails because id client is missing', () => {
+    const booking = {
+        state:"BOOKED"
+    }
+    return expect(dao.createBooking(booking)).rejects.toHaveProperty('errno', 19);
+  });
+
+test('the creation fails because state is missing', () => {
+    const booking = {
+        idClient : 2
+    }
+    return expect(dao.createBooking(booking)).rejects.toHaveProperty('errno', 19);
+});
+
+test('the creation is done', () => {
+    const booking = {
+        idClient : 2,
+        state : "BOOKED"
+    }
+    return expect(dao.createBooking(booking)).resolves.toBeGreaterThanOrEqual(1);
+});
+
+//TEST DAO FUNCTION CREATE CLIENT
+
+
