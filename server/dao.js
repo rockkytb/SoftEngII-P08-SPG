197164fbf678bdb5a5749a8a13dc1e762a1df4c3
SERSE <>
2021-11-07 17:35:01 +0100

    getbyemail and getclients

--- server/dao.js
+++ server/dao.js
@@ -1,31 +1,64 @@
-'use strict';
+"use strict";
 /* Data Access Object (DAO) module for accessing tasks */
 
-const sqlite = require('sqlite3');
-const bcrypt = require('bcrypt');
+const sqlite = require("sqlite3");
+const bcrypt = require("bcrypt");
 
 //Set to true to enable testdatabase
 const testmode = true;
 
 // open the database
-const db = new sqlite.Database(testmode ? ('testdatabase.db'):('database.db'), (err) => {
-    if(err) throw err;
-  });
+const db = new sqlite.Database(
+  testmode ? "testdatabase.db" : "database.db",
+  (err) => {
+    if (err) throw err;
+  }
+);
 
+//get Clients
+exports.getClients = () => {
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM CLIENT";
+    db.get_all(sql, [], (err, rows) => {
+      if (err) {
+        reject(err);
+      } else if (rows === undefined) {
+        resolve(false);
+      } else {
+        resolve(
+          rows.map((x) => {
+            return {
+              id: `C${x.ID}`,
+              username: x.EMAIL,
+              name: x.NAME,
+              surname: x.SURNAME,
+            };
+          })
+        );
+      }
+    });
+  });
+};
 
 //get Client
-exports.getClient = (email,password) => {
+exports.getClient = (email, password) => {
   return new Promise((resolve, reject) => {
-    const sql= 'SELECT * FROM CLIENT WHERE EMAIL = ?';
-    db.get(sql, [email], (err,row) => {
-      if(err) {reject(err);}
-      else if (row===undefined) {resolve (false);}
-      else {
-        const user = {id: `C${row.ID}`, username: row.EMAIL, name: row.NAME, surname: row.SURNAME};
+    const sql = "SELECT * FROM CLIENT WHERE EMAIL = ?";
+    db.get(sql, [email], (err, row) => {
+      if (err) {
+        reject(err);
+      } else if (row === undefined) {
+        resolve(false);
+      } else {
+        const user = {
+          id: `C${row.ID}`,
+          username: row.EMAIL,
+          name: row.NAME,
+          surname: row.SURNAME,
+        };
 
-        bcrypt.compare(password, row.PASSWORD).then(result => {
-          if(result) resolve(user);
-          
+        bcrypt.compare(password, row.PASSWORD).then((result) => {
+          if (result) resolve(user);
           else resolve(false);
         });
       }
@@ -36,12 +69,32 @@ exports.getClient = (email,password) => {
 //get Client by Id
 exports.getClientById = (id) => {
   return new Promise((resolve, reject) => {
-    const sql= 'SELECT * FROM CLIENT WHERE ID = ?';
-    db.get(sql, [id], (err,row) => {
-      if(err) {reject(err);}
-      else if (row===undefined) {resolve (false);}
-      else {
-        const user = {id: `C${row.ID}`, username: row.EMAIL};
+    const sql = "SELECT * FROM CLIENT WHERE ID = ?";
+    db.get(sql, [id], (err, row) => {
+      if (err) {
+        reject(err);
+      } else if (row === undefined) {
+        resolve(false);
+      } else {
+        const user = { id: `C${row.ID}`, username: row.EMAIL };
+        resolve(user);
+      }
+    });
+  });
+};
+
+//get Client by Email
+exports.getClientByEmail = (email) => {
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM CLIENT WHERE EMAIL = ?";
+    db.get(sql, [email], (err, row) => {
+      if (err) {
+        reject(err);
+      } else if (row === undefined) {
+        const user = { id: -1 };
+        resolve(user);
+      } else {
+        const user = { id: `C${row.ID}` };
         resolve(user);
       }
     });
@@ -49,18 +102,19 @@ exports.getClientById = (id) => {
 };
 
 //get Farmer
-exports.getFarmer = (email,password) => {
+exports.getFarmer = (email, password) => {
   return new Promise((resolve, reject) => {
-    const sql= 'SELECT * FROM FARMER WHERE EMAIL = ?';
-    db.get(sql, [email], (err,row) => {
-      if(err) {reject(err);}
-      else if (row===undefined) {resolve (false);}
-      else {
-        const user = {id: `F${row.ID}`, username: row.EMAIL};
-        
-        bcrypt.compare(password, row.PASSWORD).then(result => {
-          if(result) resolve(user);
-          
+    const sql = "SELECT * FROM FARMER WHERE EMAIL = ?";
+    db.get(sql, [email], (err, row) => {
+      if (err) {
+        reject(err);
+      } else if (row === undefined) {
+        resolve(false);
+      } else {
+        const user = { id: `F${row.ID}`, username: row.EMAIL };
+
+        bcrypt.compare(password, row.PASSWORD).then((result) => {
+          if (result) resolve(user);
           else resolve(false);
         });
       }
@@ -68,20 +122,20 @@ exports.getFarmer = (email,password) => {
   });
 };
 
-
 //get Shop Employee
-exports.getShopEmployee = (email,password) => {
+exports.getShopEmployee = (email, password) => {
   return new Promise((resolve, reject) => {
-    const sql= 'SELECT * FROM SHOP_EMPLOYEE WHERE EMAIL = ?';
-    db.get(sql, [email], (err,row) => {
-      if(err) {reject(err);}
-      else if (row===undefined) {resolve (false);}
-      else {
-        const user = {id: `S${row.ID}`, username: row.EMAIL};
-        
-        bcrypt.compare(password, row.PASSWORD).then(result => {
-          if(result) resolve(user);
-          
+    const sql = "SELECT * FROM SHOP_EMPLOYEE WHERE EMAIL = ?";
+    db.get(sql, [email], (err, row) => {
+      if (err) {
+        reject(err);
+      } else if (row === undefined) {
+        resolve(false);
+      } else {
+        const user = { id: `S${row.ID}`, username: row.EMAIL };
+
+        bcrypt.compare(password, row.PASSWORD).then((result) => {
+          if (result) resolve(user);
           else resolve(false);
         });
       }
@@ -92,12 +146,14 @@ exports.getShopEmployee = (email,password) => {
 //get Farmer by Id
 exports.getFarmerById = (id) => {
   return new Promise((resolve, reject) => {
-    const sql= 'SELECT * FROM FARMER WHERE ID = ?';
-    db.get(sql, [id], (err,row) => {
-      if(err) {reject(err);}
-      else if (row===undefined) {resolve (false);}
-      else {
-        const user = {id: `F${row.ID}`, username: row.EMAIL};
+    const sql = "SELECT * FROM FARMER WHERE ID = ?";
+    db.get(sql, [id], (err, row) => {
+      if (err) {
+        reject(err);
+      } else if (row === undefined) {
+        resolve(false);
+      } else {
+        const user = { id: `F${row.ID}`, username: row.EMAIL };
         resolve(user);
       }
     });
@@ -107,12 +163,14 @@ exports.getFarmerById = (id) => {
 //get ShopEmployee by Id
 exports.getShopEmployeeById = (id) => {
   return new Promise((resolve, reject) => {
-    const sql= 'SELECT * FROM SHOP_EMPLOYEE WHERE ID = ?';
-    db.get(sql, [id], (err,row) => {
-      if(err) {reject(err);}
-      else if (row===undefined) {resolve (false);}
-      else {
-        const user = {id: `S${row.ID}`, username: row.EMAIL};
+    const sql = "SELECT * FROM SHOP_EMPLOYEE WHERE ID = ?";
+    db.get(sql, [id], (err, row) => {
+      if (err) {
+        reject(err);
+      } else if (row === undefined) {
+        resolve(false);
+      } else {
+        const user = { id: `S${row.ID}`, username: row.EMAIL };
         resolve(user);
       }
     });
@@ -122,7 +180,7 @@ exports.getShopEmployeeById = (id) => {
 // add a new booking
 exports.createBooking = (booking) => {
   return new Promise((resolve, reject) => {
-    const sql = 'INSERT INTO BOOKING (CLIENT_ID, STATE) VALUES(?, ?)';
+    const sql = "INSERT INTO BOOKING (CLIENT_ID, STATE) VALUES(?, ?)";
     db.run(sql, [booking.idClient, booking.state], function (err) {
       if (err) {
         reject(err);
@@ -136,13 +194,18 @@ exports.createBooking = (booking) => {
 // add a new client
 exports.createClient = (client) => {
   return new Promise((resolve, reject) => {
-    const sql = 'INSERT INTO CLIENT (EMAIL, NAME, SURNAME, PASSWORD) VALUES(?, ?, ?, ?)';
-    db.run(sql, [client.email, client.name, client.surname, client.password], function (err) {
-      if (err) {
-        reject(err);
-        return;
+    const sql =
+      "INSERT INTO CLIENT (EMAIL, NAME, SURNAME, PASSWORD) VALUES(?, ?, ?, ?)";
+    db.run(
+      sql,
+      [client.email, client.name, client.surname, client.password],
+      function (err) {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(this.lastID);
       }
-      resolve(this.lastID);
-    });
+    );
   });
 };
--- server/server.js
+++ server/server.js
@@ -1,37 +1,38 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const morgan = require('morgan'); // logging middleware
-const {check, validationResult} = require('express-validator'); // validation middleware
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const session = require ('express-session');
-const dao = require('./dao'); // module for accessing the DB
+const express = require("express");
+const morgan = require("morgan"); // logging middleware
+const { check, validationResult } = require("express-validator"); // validation middleware
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const session = require("express-session");
+const dao = require("./dao"); // module for accessing the DB
 //Per validazione aggiuntiva
-const validator = require('validator');
+const validator = require("validator");
 
 // init express
 const app = new express();
 const port = 3001;
 
 // set-up the middlewares
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
 // custom middleware: check if a given request is coming from an authenticated user
 const isLoggedIn = (req, res, next) => {
-  if(req.isAuthenticated())
-    return next();
-  
-  return res.status(401).json({ error: 'not authenticated'});
-}
+  if (req.isAuthenticated()) return next();
+
+  return res.status(401).json({ error: "not authenticated" });
+};
 
 // enable sessions in Express
-app.use(session ({
-  secret:"secret phrase choosen by me",
-  resave:false,
-  saveUninitialized: false,
-}));
+app.use(
+  session({
+    secret: "secret phrase choosen by me",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 //init passport to use sessions
 app.use(passport.initialize());
@@ -39,228 +40,272 @@ app.use(passport.session());
 
 /*** PASSPORT METHODS ***/
 
-passport.use('client-local',new LocalStrategy(
-  function(username, password, done){
-    dao.getClient(username,password)
-    .then((user) => {
-      if(!user)
-        return done(null,false, {message: "Incorrect username and/or password"});
-      return done(null,user);    
-    })
-    .catch((err)=>{
-      return done(err);
-    });
-  }));
-
-  passport.use('farmer-local',new LocalStrategy(
-    function(username, password, done){
-      dao.getFarmer(username,password)
+passport.use(
+  "client-local",
+  new LocalStrategy(function (username, password, done) {
+    dao
+      .getClient(username, password)
       .then((user) => {
-        if(!user)
-          return done(null,false, {message: "Incorrect username and/or password"});
-        return done(null,user);    
+        if (!user)
+          return done(null, false, {
+            message: "Incorrect username and/or password",
+          });
+        return done(null, user);
       })
-      .catch((err)=>{
+      .catch((err) => {
         return done(err);
       });
-    }));
+  })
+);
 
-    passport.use('shop-employee-local',new LocalStrategy(
-      function(username, password, done){
-        dao.getShopEmployee(username,password)
-        .then((user) => {
-          if(!user)
-            return done(null,false, {message: "Incorrect username and/or password"});
-          return done(null,user);    
-        })
-        .catch((err)=>{
-          return done(err);
-        });
-      }));
+passport.use(
+  "farmer-local",
+  new LocalStrategy(function (username, password, done) {
+    dao
+      .getFarmer(username, password)
+      .then((user) => {
+        if (!user)
+          return done(null, false, {
+            message: "Incorrect username and/or password",
+          });
+        return done(null, user);
+      })
+      .catch((err) => {
+        return done(err);
+      });
+  })
+);
 
-passport.serializeUser((user,done) => {
-  done(null,user.id);
+passport.use(
+  "shop-employee-local",
+  new LocalStrategy(function (username, password, done) {
+    dao
+      .getShopEmployee(username, password)
+      .then((user) => {
+        if (!user)
+          return done(null, false, {
+            message: "Incorrect username and/or password",
+          });
+        return done(null, user);
+      })
+      .catch((err) => {
+        return done(err);
+      });
+  })
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
 });
 
-passport.deserializeUser((id,done) => {
-
+passport.deserializeUser((id, done) => {
   //Catch first letter from ID and decide what kind of user we have
   const type = id.charAt(0);
   const identifier = id.substring(1);
 
-  if(type === 'C'){
-    dao.getClientById(identifier).then((user) => {
-      done(null,user);
-    })
-    .catch((err) => {
-      done(err,null)
-    });
+  if (type === "C") {
+    dao
+      .getClientById(identifier)
+      .then((user) => {
+        done(null, user);
+      })
+      .catch((err) => {
+        done(err, null);
+      });
   }
-  if (type === 'F'){
-    dao.getFarmerById(identifier).then((user) => {
-      done(null,user);
-    })
-    .catch((err) => {
-      done(err,null)
-    });
+  if (type === "F") {
+    dao
+      .getFarmerById(identifier)
+      .then((user) => {
+        done(null, user);
+      })
+      .catch((err) => {
+        done(err, null);
+      });
   }
-  if (type === 'S'){
-    dao.getShopEmployeeById(identifier).then((user) => {
-      done(null,user);
-    })
-    .catch((err) => {
-      done(err,null)
-    });
+  if (type === "S") {
+    dao
+      .getShopEmployeeById(identifier)
+      .then((user) => {
+        done(null, user);
+      })
+      .catch((err) => {
+        done(err, null);
+      });
   }
-
-  
 });
 
 //WRITE API HERE
 
 //POST /api/clientSessions FOR LOGIN OF CLIENT
-app.post('/api/clientSessions',function(req, res, next) {
-  passport.authenticate('client-local', (err, user, info) => {
-    if (err)
-      return next(err);
-      if (!user) {
-        // display wrong login messages
-        return res.status(401).json(info);
-      }
-      // success, perform the login
-      req.login(user, (err) => {
-        if (err)
-          return next(err);
-        
-        // req.user contains the authenticated user, we send all the user info back
-        // this is coming from userDao.getUser()
-        return res.json(req.user);
-      });
+app.post("/api/clientSessions", function (req, res, next) {
+  passport.authenticate("client-local", (err, user, info) => {
+    if (err) return next(err);
+    if (!user) {
+      // display wrong login messages
+      return res.status(401).json(info);
+    }
+    // success, perform the login
+    req.login(user, (err) => {
+      if (err) return next(err);
+
+      // req.user contains the authenticated user, we send all the user info back
+      // this is coming from userDao.getUser()
+      return res.json(req.user);
+    });
   })(req, res, next);
 });
 
 //POST /api/famerSessions FOR LOGIN OF FARMERS
-app.post('/api/farmerSessions',function(req, res, next) {
-  passport.authenticate('farmer-local', (err, user, info) => {
-    if (err)
-      return next(err);
-      if (!user) {
-        // display wrong login messages
-        return res.status(401).json(info);
-      }
-      // success, perform the login
-      req.login(user, (err) => {
-        if (err)
-          return next(err);
-        
-        // req.user contains the authenticated user, we send all the user info back
-        // this is coming from userDao.getUser()
-        return res.json(req.user);
-      });
+app.post("/api/farmerSessions", function (req, res, next) {
+  passport.authenticate("farmer-local", (err, user, info) => {
+    if (err) return next(err);
+    if (!user) {
+      // display wrong login messages
+      return res.status(401).json(info);
+    }
+    // success, perform the login
+    req.login(user, (err) => {
+      if (err) return next(err);
+
+      // req.user contains the authenticated user, we send all the user info back
+      // this is coming from userDao.getUser()
+      return res.json(req.user);
+    });
   })(req, res, next);
 });
 
 //POST /api/shopEmployeeSessions FOR LOGIN OF SHOP EMPLOYEE
-app.post('/api/shopEmployeeSessions',function(req, res, next) {
-  passport.authenticate('shop-employee-local', (err, user, info) => {
-    if (err)
-      return next(err);
-      if (!user) {
-        // display wrong login messages
-        return res.status(401).json(info);
-      }
-      // success, perform the login
-      req.login(user, (err) => {
-        if (err)
-          return next(err);
-        
-        // req.user contains the authenticated user, we send all the user info back
-        // this is coming from userDao.getUser()
-        return res.json(req.user);
-      });
+app.post("/api/shopEmployeeSessions", function (req, res, next) {
+  passport.authenticate("shop-employee-local", (err, user, info) => {
+    if (err) return next(err);
+    if (!user) {
+      // display wrong login messages
+      return res.status(401).json(info);
+    }
+    // success, perform the login
+    req.login(user, (err) => {
+      if (err) return next(err);
+
+      // req.user contains the authenticated user, we send all the user info back
+      // this is coming from userDao.getUser()
+      return res.json(req.user);
+    });
   })(req, res, next);
 });
 
 //POST /api/logout
-app.post("/api/logout", (req,res)=>{
+app.post("/api/logout", (req, res) => {
   req.logout();
   res.end();
 });
 
 //GET /api/userinfo
-app.get("/api/userinfo", isLoggedIn, (req,res)=>{
+app.get("/api/userinfo", isLoggedIn, (req, res) => {
   res.status(200).json(req.user);
 });
 
-//POST /api/bookings
-app.post('/api/bookings', //isLoggedIn,
-    async (req, res) => {
+//GET /api/clients/
+app.get("/api/clients", isLoggedIn, (req, res) => {
+  dao
+    .getClients()
+    .then((clients) => {
+      res.status(200).json(clients);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        errors: `Database errors: ${err}.`,
+      });
+    });
+});
 
-    if(!validator.isInt(`${req.body.idClient}`,{min:0})){
-      return res.status(422).json({error: `Invalid client id, it must be positive`});
+//GET /api/client/
+app.get("/api/client", isLoggedIn, (req, res) => {
+  dao
+    .getClientByEmail(req.email)
+    .then((client) => {
+      res.status(200).json(client);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        errors: `Database errors: ${err}.`,
+      });
+    });
+});
+
+//POST /api/bookings
+app.post(
+  "/api/bookings", //isLoggedIn,
+  async (req, res) => {
+    if (!validator.isInt(`${req.body.idClient}`, { min: 0 })) {
+      return res
+        .status(422)
+        .json({ error: `Invalid client id, it must be positive` });
     }
-    
+
     const booking = {
       idClient: req.body.idClient,
-      state : 'BOOKED'
+      state: "BOOKED",
     };
 
     let bookingId;
-  
+
     try {
-      bookingId=await dao.createBooking(booking);
-      
-    } catch(err) {
-      res.status(503).json({error: `Database error during the creation of booking for client: ${booking.idClient}.`});
+      bookingId = await dao.createBooking(booking);
+    } catch (err) {
+      res.status(503).json({
+        error: `Database error during the creation of booking for client: ${booking.idClient}.`,
+      });
     }
 
     //All went fine
-    res.status(201).json({idBooking: bookingId});
-});
+    res.status(201).json({ idBooking: bookingId });
+  }
+);
 
 //POST /api/newclient
-app.post('/api/newclient', //isLoggedIn,
-    async (req, res) => {
-
-    if(!validator.isEmail(req.body.email)){
-      return res.status(422).json({error: `Invalid client's email`});
+app.post(
+  "/api/newclient", //isLoggedIn,
+  async (req, res) => {
+    if (!validator.isEmail(req.body.email)) {
+      return res.status(422).json({ error: `Invalid client's email` });
     }
 
-    if(!validator.isLength(req.body.name,{min:1, max:100})){
-      return res.status(422).json({error: `Invalid client's name`});
+    if (!validator.isLength(req.body.name, { min: 1, max: 100 })) {
+      return res.status(422).json({ error: `Invalid client's name` });
     }
 
-    if(!validator.isLength(req.body.surname,{min:1, max:100})){
-      return res.status(422).json({error: `Invalid client's surname`});
+    if (!validator.isLength(req.body.surname, { min: 1, max: 100 })) {
+      return res.status(422).json({ error: `Invalid client's surname` });
     }
-    
-    if(!validator.isLength(req.body.password,{min:60, max:60})){
-      return res.status(422).json({error: `Invalid client's password hash`});
+
+    if (!validator.isLength(req.body.password, { min: 60, max: 60 })) {
+      return res.status(422).json({ error: `Invalid client's password hash` });
     }
 
     const client = {
       email: req.body.email,
-      name : req.body.name,
-      surname : req.body.surname,
-      password: req.body.password
+      name: req.body.name,
+      surname: req.body.surname,
+      password: req.body.password,
     };
 
     let clientId;
-  
+
     try {
-      clientId=await dao.createClient(client);
-      
-    } catch(err) {
-      res.status(503).json({error: `Database error during the creation of client: ${client.email}.`});
+      clientId = await dao.createClient(client);
+    } catch (err) {
+      res.status(503).json({
+        error: `Database error during the creation of client: ${client.email}.`,
+      });
     }
 
     //All went fine
-    res.status(201).json({idClient: clientId});
-});
-
-
+    res.status(201).json({ idClient: clientId });
+  }
+);
 
 // activate the server
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
-}); 
+});
