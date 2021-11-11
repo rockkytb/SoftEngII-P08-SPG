1cf7a13b4c7272aae9967e6ea0f9746507b060c2
Rocco Luca Iamello s282145 <roccoluca@hotmail.it>
2021-11-11 18:37:05 +0100

    removed console log

--- server/dao.js
+++ server/dao.js
@@ -126,7 +126,6 @@ exports.getFarmer = (email, password) => {
 exports.getShopEmployee = (email, password) => {
   return new Promise((resolve, reject) => {
     const sql = "SELECT * FROM SHOP_EMPLOYEE WHERE EMAIL = ?";
-    console.log(email + " " + password);
     db.get(sql, [email], (err, row) => {
       if (err) {
         reject(err);
