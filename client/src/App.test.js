d307e87a7a3091f1336c10477834c869be3b9b12
Rocco Luca Iamello s282145 <roccoluca@hotmail.it>
2021-11-12 15:18:57 +0100

    Add sketch for frontend test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Enzyme, { shallow, render, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import NewClientForm from './NewClientForm.js';
+Enzyme.configure({ adapter: new Adapter() })
 
 
 test('renders learn react link', () => {
-  //render(<App />);
+  shallow(<NewClientForm/>);
   //const linkElement = screen.getByText(/learn react/i);
   //expect(linkElement).toBeInTheDocument();
 })
