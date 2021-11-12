d2d83b5f696fca502efa1ac7ca2d946c3640c355
Rocco Luca Iamello s282145 <roccoluca@hotmail.it>
2021-11-12 16:48:49 +0100

    Finally test environment for frontend

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import App from './App.js';
 import NewClientForm from './NewClientForm.js';
 import Employee from './Employee.js';
 import Customer from './Customer.js';
 import SidebarCustom from './Sidebar.js';
 Enzyme.configure({ adapter: new Adapter() })
 
+test('renders app', () => {
+  shallow(<App/>);
+});
 
 test('renders new client form', () => {
   shallow(<NewClientForm/>);
