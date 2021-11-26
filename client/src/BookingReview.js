11f033b90311e631b26b26a3e882353dbcfb2c05
Rocco Luca Iamello s282145 <roccoluca@hotmail.it>
2021-11-26 10:13:39 +0100

    Refactored booking

--- client/src/BookingReview.js
+++ client/src/BookingReview.js
@@ -21,6 +21,17 @@ function BookingReview(props) {
   const [show, setShow] = useState(false);
   const [soldy, setSoldy] = useState(0);
 
+    
+  const [deliveryMode, setDeliveryMode] = useState(false);
+  const [street, setStreet] = useState (null);
+  const [city, setCity] = useState (null);
+  const [province, setProvince] = useState (null);
+  const [postalCode, setPostalCode] = useState (null);
+  const [country, setCountry] = useState (null);
+  const [date, setDate] = useState ("");
+  const [time, setTime] = useState ("");
+  const [extraFee, setExtraFee] = useState (0);
+
   async function newBooking(IDclient) {
     // DA VERIFICARE CON API È PER INSERIRE UN NUOVO BOOKING. MANDA ALL'API IL CLIENTID PRESO DAL BOOKING
     // sì però stai calmo
@@ -66,6 +77,8 @@ function BookingReview(props) {
   }
 
   const handleClose = () => {
+    setDate("");
+    setTime("");
     setShow(false);
   };
 
@@ -182,8 +195,9 @@ function BookingReview(props) {
         <Modal.Body>
           <Col>
             <Form>
+              { props.userdata.id.charAt(0) === "S" &&
               <Form.Group>
-                <Form.Label>Select Client</Form.Label>
+                <Form.Label><h3>Select Client</h3></Form.Label>
                 <Form.Control
                   as="select"
                   onChange={(e) => setClientID(e.target.value)}
@@ -199,6 +213,57 @@ function BookingReview(props) {
                   )}
                 </Form.Control>
               </Form.Group>
+              }
+              <Form.Label><h3>Delivery Options</h3></Form.Label>
+
+                <Row className="text-center">
+                    <Col xs={2} />
+                    <Col xs={8}>
+                        <Form.Group id="DeliveryMode">
+                            <Form.Check type="checkbox" label="Delivery at home?" 
+                            checked={deliveryMode} 
+                            onChange={() => setDeliveryMode(!deliveryMode)} />
+                         </Form.Group>
+                    </Col>
+                    <Col xs={2} />
+                </Row>
+
+                <Form.Label>
+                    <h6> Pick-Up in Shop </h6>
+                </Form.Label>
+                
+                <Row>
+                    <Col xs={2} />
+                    <Col xs={8}>
+                        <Form.Group size="lg" controlId="date">
+                            <Form.Label>Date: </Form.Label>
+                            <Form.Control 
+                                    type="date" 
+                                    value={date}
+                                    required 
+                                    onChange={ev => setDate(ev.target.value)} />
+                        </Form.Group>
+                    </Col>
+                    <Col xs={2} />
+                </Row>
+
+                <Row>
+                    <Col xs={2} />
+                    <Col xs={8}>
+                        <Form.Group size="lg" controlId="time">
+                            <Form.Label>Time: </Form.Label>
+                            <Form.Control 
+                                    type="time" 
+                                    value={time} 
+                                    required
+                                    onChange={ev => setTime(ev.target.value)} />
+                        </Form.Group>
+                    </Col>
+                    <Col xs={2} />
+                </Row>
+              
+
+
             </Form>
           </Col>
         </Modal.Body>
@@ -206,11 +271,13 @@ function BookingReview(props) {
           <Button variant="secondary" id="closeModal" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" id="submitModal" onClick={() => handleCreateBooking()}>
+          <Button variant="primary" id="submitModal" onClick={() => {handleCreateBooking()}}>
             Submit
           </Button>
         </Modal.Footer>
       </Modal>
+
+      
 
       <CardColumns xs={1} md={5}>
         <>{props.cart.length ? productsActions() : <></>}</>
@@ -226,12 +293,9 @@ function BookingReview(props) {
             </Col>
             <Col className="md-2 text-left">
                 <Button variant="primary" id="butConf" onClick={() => {
-                    if(props.userdata.id.charAt(0) === "S"){
+                    
                       setShow(true);
-                    }
-                    else{
-                      handleCreateBooking();
-                    }
+                    
                     }}>
                   Confirm Booking
                 </Button>
