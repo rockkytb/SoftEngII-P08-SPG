c33169643607758da7fcf63e02f803aa08043cef
Rocco Luca Iamello s282145 <roccoluca@hotmail.it>
2021-12-08 19:55:55 +0100

    Filters interface

--- client/src/ProductsList.js
+++ client/src/ProductsList.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { CardColumns, Card, Col, Button, Modal, Form } from "react-bootstrap";
+import { CardColumns, Card, Row,Col, Button, Modal, Form } from "react-bootstrap";
+import Dropdown from "react-bootstrap/Dropdown";
 
 export default function ProductsList(props) {
   const [showView, setShowView] = useState(false);
@@ -11,7 +12,15 @@ export default function ProductsList(props) {
   const [quantity, setQuantity] = useState(1);
   const [price, setPrice] = useState(0);
 
+  //FILTERS
+  const[categoryFilter,setCategoryFilter]= useState([]);
+  const[farmerFilter,setFarmerFilter]= useState([]);
+
   const [orderQuantity, setOrderQuantity] = useState(1);
+
+
+  const availableCategories = Array.from(new Set(props.products.map(p=>p.category)));
+  const availableFarmers = Array.from(new Set(props.products.map(p=>p.farmer_email)));
 
   function handleAddToCart() {
     let update = 0;
@@ -74,20 +83,8 @@ export default function ProductsList(props) {
     setQuantity(1);
   };
 
+  //DEFAULT, SORTED IN ALPHABETIC ORDER
   function productsActions() {
-    //@kricar gioca a europa universalis anziché a tempo di boom :c
-    /*console.log(props.products.sort((a, b) => {
-      var nameA = a.name.toUpperCase(); // ignore upper and lowercase
-      var nameB = b.name.toUpperCase(); // ignore upper and lowercase
-      if (nameA < nameB) {
-        return -1;
-      }
-      if (nameA > nameB) {
-        return 1;
-      }
-      // names must be equal
-      return a.id - b.id;
-    }));*/
     //TOCHECK: problems into modifying a prop?
     //the product must be one from next week so it must be not already confirmed
     return props.products.sort((a, b) => {
@@ -139,12 +136,75 @@ export default function ProductsList(props) {
 
   return (
     <>
-      <div className="below-nav ">
-        <div class="col-md-12 text-center">
-          <Link to={"/newOrder"}>
-            <Button variant="warning" className="mr-2 md-2 ">Go to cart</Button>
-          </Link>
-        </div>
+      <div className="below-nav no-flickr">
+        <Row className="pb-4">
+          <Col md={3} xs={4} className="pr-1 text-right">
+            <Dropdown>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                Category
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                {availableCategories.map((c,catid)=>(<>
+                  <Dropdown.Item onClick={() => {
+                        if(categoryFilter.includes(c)){
+                          setCategoryFilter(oldList => {return oldList.filter(cat => cat !== c);});
+                        }
+                        else{
+                          setCategoryFilter(oldList => {return oldList.concat(c);});
+                        }
+                      
+                        }}>
+                  <Form.Group id={catid}>
+                    <Form.Check
+                      type="checkbox"
+                      label={c}
+                      checked={categoryFilter.includes(c)}
+                    />
+                  </Form.Group>
+                </Dropdown.Item>
+                </>))}
+                
+                
+              </Dropdown.Menu>
+            </Dropdown>
+          </Col>
+          <Col md={3} xs={4} className="text-left">
+            <Dropdown>
+                <Dropdown.Toggle variant="success" id="dropdown-basic">
+                  Farmer
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                {availableFarmers.map((f,farmerid)=>(<>
+                  <Dropdown.Item onClick={() => {
+                        console.log(farmerFilter);
+                        if(farmerFilter.includes(f)){
+                          setFarmerFilter(oldList => {return oldList.filter(farmer => farmer !== f);});
+                        }
+                        else{
+                          setFarmerFilter(oldList => {return oldList.concat(f);});
+                        }
+                      
+                        }}>
+                  <Form.Group id={farmerid}>
+                    <Form.Check
+                      type="checkbox"
+                      label={f}
+                      checked={farmerFilter.includes(f)}
+                    />
+                  </Form.Group>
+                </Dropdown.Item>
+                </>))}
+                </Dropdown.Menu>
+            </Dropdown>
+          </Col>
+          <Col md={6} xs={4} className="text-left p-0">
+            <Link to={"/newOrder"}>
+              <Button variant="warning" className="mr-2 md-2 ">View cart</Button>
+            </Link>
+          </Col>
+        </Row>
         <Modal show={showView} onHide={handleViewClose}>
           <Modal.Header>
             <Modal.Title>Add to cart</Modal.Title>
