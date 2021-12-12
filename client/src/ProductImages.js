5481ac77f987db22de05db7ba45754a3e6dae4cb
Rocco Luca Iamello s282145 <roccoluca@hotmail.it>
2021-12-12 15:02:45 +0100

    Other products images

--- client/src/ProductImages.js
+++ client/src/ProductImages.js
@@ -10,7 +10,13 @@ import  cauliflower  from "./product_images/cauliflower.png";
 import  cherry  from "./product_images/cherry.png";
 import  chicken_breast  from "./product_images/chicken breast.png";
 import  coconut  from "./product_images/coconut.png";
+import  corn  from "./product_images/corn.png";
+import  cucumber  from "./product_images/cucumber.png";
+import  eggplants  from "./product_images/eggplants.png";
+import  eggs  from "./product_images/eggs.png";
+import  flour_type_0  from "./product_images/flour type 0.png";
+import  flour_type_00  from "./product_images/flour type 00.png";
 import  def  from "./product_images/default.png";
 
 export default [def,apple,artichoke, asparagus, avocado, banana, blueberries, broccoli, carrots, cauliflower,
-    cherry, chicken_breast, coconut];
+    cherry, chicken_breast, coconut, corn, cucumber, eggplants, eggs, flour_type_0, flour_type_00];
