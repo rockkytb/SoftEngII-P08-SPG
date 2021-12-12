526252e76a7937c34776d6ae5af642cd742c1ce8
Rocco Luca Iamello s282145 <roccoluca@hotmail.it>
2021-12-12 11:24:24 +0100

    Add some products images

--- client/src/ProductImages.js
+++ client/src/ProductImages.js
@@ -1,4 +1,16 @@
 import  apple  from "./product_images/apples.png";
+import  artichoke  from "./product_images/artichoke.png";
+import  asparagus  from "./product_images/asparagus.png";
+import  avocado  from "./product_images/avocado.png";
+import  banana  from "./product_images/banana.png";
+import  blueberries  from "./product_images/blueberries.png";
+import  broccoli  from "./product_images/broccoli.png";
+import  carrots  from "./product_images/carrots.png";
+import  cauliflower  from "./product_images/cauliflower.png";
+import  cherry  from "./product_images/cherry.png";
+import  chicken_breast  from "./product_images/chicken breast.png";
+import  coconut  from "./product_images/coconut.png";
 import  def  from "./product_images/default.png";
 
-export default [def,apple];
+export default [def,apple,artichoke, asparagus, avocado, banana, blueberries, broccoli, carrots, cauliflower,
+    cherry, chicken_breast, coconut];
