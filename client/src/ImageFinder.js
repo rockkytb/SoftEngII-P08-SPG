31cfafd946c8642efcbace391508453171722e79
Rocco Luca Iamello s282145 <roccoluca@hotmail.it>
2021-12-13 10:31:44 +0100

    Fix all frontend tests

--- client/src/ImageFinder.js
+++ client/src/ImageFinder.js
@@ -1,9 +1,21 @@
 import ProductImages from "./ProductImages";
 
 export default function ImageFinder (name){
-    const img = ProductImages.find(i => i.includes(name));
+    const img = ProductImages.find(i => {
+        if(typeof i === "string")
+        {
+            
+            return i.includes(name);
+        }
+    });
     if (img)
         return img;
     else
-        return ProductImages.find(i => i.includes("default"));
+        return ProductImages.find(i => 
+            {
+                if(typeof i === "string")
+                {         
+                    return i.includes("default");
+                }
+            });
   }
