0bd9cb27aeedc1d556c2324c6e534643890b533d
Rocco Luca Iamello s282145 <roccoluca@hotmail.it>
2021-12-13 09:38:46 +0100

    All product images

--- client/src/ProductImages.js
+++ client/src/ProductImages.js
@@ -16,7 +16,44 @@ import  eggplants  from "./product_images/eggplants.png";
 import  eggs  from "./product_images/eggs.png";
 import  flour_type_0  from "./product_images/flour type 0.png";
 import  flour_type_00  from "./product_images/flour type 00.png";
+import  garlic  from "./product_images/garlic.png";
+import  grape  from "./product_images/grape.png";
+import  green_pepper  from "./product_images/green pepper.png";
+import  kiwi  from "./product_images/kiwi.png";
+import  lemons  from "./product_images/lemons.png";
+import  lettuce  from "./product_images/lettuce.png";
+import  lime  from "./product_images/lime.png";
+import  mango  from "./product_images/mango.png";
+import  melon  from "./product_images/melon.png";
+import  milk  from "./product_images/milk.png";
+import  mushrooms  from "./product_images/mushrooms.png";
+import  onion  from "./product_images/onion.png";
+import  oranges  from "./product_images/oranges.png";
+import  papaya  from "./product_images/papaya.png";
+import  parsley  from "./product_images/parsley.png";
+import  passion_fruit  from "./product_images/passion fruit.png";
+import  peach  from "./product_images/peach.png";
+import  pears  from "./product_images/pears.png";
+import  peas  from "./product_images/peas.png";
+import  pineapple  from "./product_images/pineapple.png";
+import  pork_sausage  from "./product_images/pork sausage.png";
+import  potato  from "./product_images/potato.png";
+import  pumpkin  from "./product_images/pumpkin.png";
+import  radish  from "./product_images/radish.png";
+import  raspberry  from "./product_images/raspberry.png";
+import  red_chillies  from "./product_images/red chillies.png";
+import  spinach  from "./product_images/spinach.png";
+import  strawberries  from "./product_images/strawberries.png";
+import  sweet_potato  from "./product_images/sweet potato.png";
+import  tomatoes  from "./product_images/tomatoes.png";
+import  watermelon  from "./product_images/watermelon.png";
+import  zucchini  from "./product_images/zucchini.png";
+
+//Default image
 import  def  from "./product_images/default.png";
 
 export default [def,apple,artichoke, asparagus, avocado, banana, blueberries, broccoli, carrots, cauliflower,
-    cherry, chicken_breast, coconut, corn, cucumber, eggplants, eggs, flour_type_0, flour_type_00];
+    cherry, chicken_breast, coconut, corn, cucumber, eggplants, eggs, flour_type_0, flour_type_00,
+    garlic,grape, green_pepper, kiwi, lemons, lettuce, lime, mango, melon, milk,mushrooms, 
+    onion, oranges, papaya, parsley, passion_fruit, peach, pears, peas, pineapple, pork_sausage, potato,
+    pumpkin, radish, raspberry, red_chillies, spinach, strawberries, sweet_potato, tomatoes, watermelon, zucchini  ];
