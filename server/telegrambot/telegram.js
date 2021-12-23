bccd787dcf23a935641a1132cfedea7c8f8970f3
FilippoBau <s281945@studenti.polito.it>
2021-12-23 11:27:39 +0100

    bot quasi fatto

--- server/telegrambot/telegram.js
+++ server/telegrambot/telegram.js
@@ -1,28 +1,25 @@
 // username bot @spgpolitobot
-//5081840301:AAEk17sOGoPEQ24wNCh5pQ7-YSRcY1Usyssx
+//5081840301:AAEk17sOGoPEQ24wNCh5pQ7-YSRcY1Usyss
 
-const { Telegraf } = require("telegraf");
-const TOKEN = "5081840301:AAEk17sOGoPEQ24wNCh5pQ7-YSRcY1Usyssx";
+const Telegraf = require("telegraf");
+const TOKEN = "5081840301:AAEk17sOGoPEQ24wNCh5pQ7-YSRcY1Usyss";
 const SERVER_URL = "https://c1d9-95-251-143-39.ngrok.io";
 const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
-const URI = `/webhook/${TOKEN}`;
-const WEBHOOK_URL = SERVER_URL + URI;
+
 const bot = new Telegraf(TOKEN);
 
 const Dao = require("./DaoBot.js");
 const axios = require("axios");
 
-bot.telegram.setWebhook("https://server.tld:8443/secret-path", {
-  source: "server-cert.pem",
-});
+//bot.telegram.setWebhook(SERVER_URL);
 
 bot.command("quit", (ctx) => {
   ctx.telegram.leaveChat(ctx.message.chat.id);
   ctx.leaveChat();
 });
 
-bot.command("start", async (ctx) => {
-  await Dao.InsertChatId(ctx.message.chat.id, ctx.message.from.username);
+bot.start((ctx) => {
+  // await Dao.InsertChatId(ctx.message.chat.id, ctx.message.from.username);
   console.log(ctx.message.chat.id);
   ctx.reply(`Dear ${ctx.message.from.username} Welcome`);
   ctx.reply(`Please send your number`);
@@ -38,12 +35,13 @@ bot.command("notifications", (ctx) => {
 
 bot.launch();
 // Enable graceful stop
-process.once("SIGINT", () => bot.stop("SIGINT"));
-process.once("SIGTERM", () => bot.stop("SIGTERM"));
+//process.once("SIGINT", () => bot.stop("SIGINT"));
+//process.once("SIGTERM", () => bot.stop("SIGTERM"));
+/*
 bot.on("text", async (ctx) => {
   console.log(ctx.message.text);
   if (ctx.message.text.length == 10) {
-    var x = await Dao.updateMobile(ctx.message.text, ctx.message.chat.id);
+    var x = await Dao.updateNumber(ctx.message.text, ctx.message.chat.id);
     ctx.reply(
       `Your Number is Saved, For Change it you can send phone number again in the chat`
     );
@@ -58,4 +56,4 @@ bot.on("text", async (ctx) => {
     return;
   }
   ctx.reply(`The Command is worng Please Use From Command To Help You`);
-});
+});*/
